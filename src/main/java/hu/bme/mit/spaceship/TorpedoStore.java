a0a76b2d8ceaa215e52fe598df319703526ba882
koczoa <onokocz@protonmail.com>
2024-04-26 13:36:02 +0200

    changes requested by peer made. random is private and final

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -9,7 +9,7 @@ import java.util.Random;
 */
 public class TorpedoStore {
 
-  Random generator = new Random();
+  private final Random generator = new Random();
 
   // rate of failing to fire torpedos [0.0, 1.0]
   private double FAILURE_RATE = 0.0; //NOSONAR
