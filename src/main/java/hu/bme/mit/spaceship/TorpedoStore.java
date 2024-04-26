7da136140e2100b997ff6c4b2b159d869015e701
koczoa <onokocz@protonmail.com>
2024-04-26 12:55:06 +0200

    random static

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -9,6 +9,8 @@ import java.util.Random;
 */
 public class TorpedoStore {
 
+  Random generator = new Random();
+
   // rate of failing to fire torpedos [0.0, 1.0]
   private double FAILURE_RATE = 0.0; //NOSONAR
 
@@ -36,7 +38,7 @@ public class TorpedoStore {
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
-    Random generator = new Random();
+    
     double r = generator.nextDouble();
 
     if (r >= FAILURE_RATE) {
